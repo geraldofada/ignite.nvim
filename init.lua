8b3f514855161595464d58ffb1ceb8c7715dc0c5
Geraldo Fada <geraldofada.neto@gmail.com>
2022-07-03 18:33:36 -0300

    keymaps and config init

--- init.lua
+++ init.lua
@@ -2,5 +2,9 @@ require("ignite.globals")
 
 local plugin_loader = require("ignite.plugins")
 local plugins = require("ignite.plugins.list")
+local general = require("ignite.general")
+local keymaps = require("ignite.keymaps")
 
 plugin_loader.setup(plugins)
+general.setup()
+keymaps.setup()
